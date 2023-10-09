4f3f797abfe4d522886ecb1f66f76a372f22aa97
Muthanna Alwahash <mkhuthir@gmail.com>
2023-10-09 11:45:59 +0400

    w

--- stc89c52/blink/src/delay.c
+++ stc89c52/blink/src/delay.c
@@ -1,8 +1,8 @@
 #include "delay.h"
 
-#define MAIN_Fosc 22118400L   //Define the clock
+//#define MAIN_Fosc 22118400L   //Define the clock
 //#define MAIN_Fosc 12000000L   //Define the clock
-//#define MAIN_Fosc 11059200L   //Define the clock
+#define MAIN_Fosc 11059200L   //Define the clock
 //#define MAIN_Fosc 5529600L    //Define the clock
 //#define MAIN_Fosc 24000000L   //Define the clock
 
--- stc89c52/blink/src/main.c
+++ stc89c52/blink/src/main.c
@@ -1,14 +1,9 @@
-#include <reg51.h>     // old header from SDCC
-//#include "STC15Fxx.h"  // Official header from STC-ISP
 #include <stdint.h>
 #include <stdio.h>
+#include "STC89xx.h"
 #include "delay.h"
 
-// P1 is Chip Pin #P1.0 for STC15W408AS Development board LED Pin
 #define ledPin P1
-
-// P33 is Chip Pin #P3.3 for STC15W204S Development board LED Pin
-//#define ledPin P33
 
 void main()
 {
@@ -19,10 +14,5 @@ void main()
         ledPin = 0xff;  // LED off
         delay_ms(1000);
 
-        // int n;
-        // ledPin = 0;  // LED on
-        // for (n = 0; n < 10000; n++); // waste some cycles
-        // ledPin = 1;  // LED off
-        // for (n = 0; n < 10000; n++); // waste some cycles
     }
 }
