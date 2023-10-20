1f54f3bfa51eb9d376b3cc262120d4398ab2ca6d
Muthanna Alwahash <mkhuthir@gmail.com>
2023-10-20 22:14:23 +0400

    w

--- stc/89c52rc/lcd1602/lib/lcd1602/lib_lcd1602.h
+++ stc/89c52rc/lcd1602/lib/lcd1602/lib_lcd1602.h
@@ -1,22 +1,23 @@
 //E51_LIB_LCD1602_H
 
 #ifndef E51_LIB_LCD1602_H
-#define E51_LIB_LCD1602_H
+    #define E51_LIB_LCD1602_H
 
-#define LCD_EN P1_0
-#define LCD_WR P1_1
-#define LCD_RS P1_2
-#define LCD_DATA_PORT P0
+    #define LCD_EN          P1_0
+    #define LCD_WR          P1_1
+    #define LCD_RS          P1_2
+    #define LCD_DATA_PORT   P0
 
-void lcd_write_command(unsigned char command);
-void lcd_write_data(unsigned char data);
-void lcd_1602_init(void);
-void lcd_set_ac(unsigned int line, unsigned int column);
-void lcd_show_char(unsigned int line, unsigned int column, unsigned char show_char);
-void lcd_show_string(unsigned int line, unsigned int column, unsigned char *show_string);
-void lcd_show_int_num(unsigned int line, unsigned int column, unsigned int num, unsigned int length);
-void lcd_show_dec_num(unsigned int line, unsigned int column, int num, unsigned int length);
-void lcd_show_hex_num(unsigned int line, unsigned int column, unsigned char hex_num, unsigned int length);
-void lcd_show_bin_num(unsigned int line, unsigned int column, unsigned int bin_num, unsigned int length);
+    void lcd_write_command  (unsigned char command);
+    void lcd_write_data     (unsigned char data);
+    void lcd_1602_init      (void);
+    void lcd_set_ac         (unsigned int line, unsigned int column);
+
+    void lcd_show_char      (unsigned int line, unsigned int column, unsigned char show_char);
+    void lcd_show_string    (unsigned int line, unsigned int column, unsigned char *show_string);
+    void lcd_show_int_num   (unsigned int line, unsigned int column, unsigned int num, unsigned int length);
+    void lcd_show_dec_num   (unsigned int line, unsigned int column, int num, unsigned int length);
+    void lcd_show_hex_num   (unsigned int line, unsigned int column, unsigned char hex_num, unsigned int length);
+    void lcd_show_bin_num   (unsigned int line, unsigned int column, unsigned int bin_num, unsigned int length);
 
 #endif //E51_LIB_LCD1602_H
--- stc/89c52rc/lcd1602/src/lcd.c
+++ stc/89c52rc/lcd1602/src/lcd.c
@@ -1,16 +1,16 @@
-#include "STC89xx.h"     // Official header from STC-ISP for STC89xx / STC90xx
-#include "lib_lcd1602.h"
+#include "STC89xx.h"        // Official header from STC-ISP for STC89xx / STC90xx
+#include "lib_lcd1602.h"    // pins settings are in lib_lcd1602.h
 
 void main()
 {
     lcd_1602_init();
 
-    lcd_show_char(1, 1, 'A');
-    lcd_show_bin_num(1, 3, 0xA5, 8);
-    lcd_show_string(1, 1, "Hello World!");
-    lcd_show_int_num(2, 1, 457, 3);
-    lcd_show_dec_num(2, 5, -789, 3);
-    lcd_show_hex_num(2, 10, 0xA4, 2);
+    lcd_show_char       (1, 1,  'A'                 );
+    lcd_show_string     (1, 1,  "Hello World!"      );
+    lcd_show_int_num    (2, 1,  45,3    );
+    lcd_show_dec_num    (2, 5,  -78,3   );
+    lcd_show_hex_num    (2, 10, 0xA4,2  );
+    lcd_show_bin_num    (2, 14, 0x0F,8  );
 
     while (1) {}
 }
