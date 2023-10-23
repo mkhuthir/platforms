c3d954c29d09b442dac822cf9f960f5f77489da3
Muthanna Alwahash <mkhuthir@gmail.com>
2023-10-23 21:18:43 +0400

    w

---
+++ stc/89c52rc_devboard/lcd12864/lib/lcd12864/lib_lcd12864.h
@@ -0,0 +1,30 @@
+// Muthanna Alwahash
+// Oct 2023
+
+#ifndef __REG51_H__
+#define __REG51_H__
+
+#define lcddata P0
+
+sbit EN=P2^0;
+sbit RW=P2^1;
+sbit RS=P2^2;
+sbit CS2=P2^3;
+sbit CS1=P2^4;
+
+//Basic functions
+
+extern void lcd_init(void);                             // LCD initialization
+extern void first_page(void);                           // clear screen
+
+extern void lcd_write_command(unsigned char command);   // write command to LCD
+extern void lcd_write_data(unsigned char RWdata);       // write data to LCD
+
+extern unsigned char lcd_read_data(void);
+
+
+//figure functions
+
+extern void set_dot(unsigned char x,unsigned char y);   // Set a pixel
+
+#endif
--- stc/89c52rc_devboard/lcd12864/src/lcd.c
+++ stc/89c52rc_devboard/lcd12864/src/lcd.c
@@ -2,6 +2,7 @@
 // Oct 2023
 
 #include "STC89xx.h"        // Official header from STC-ISP for STC89xx / STC90xx
+#include "lib_lcd12864.h"   // LCD 128x64 display
 
 void main()
 {
