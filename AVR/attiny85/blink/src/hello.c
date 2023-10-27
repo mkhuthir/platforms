47914003bce91bde658f9346b2beb40f0e7a5417
Muthanna Alwahash <mkhuthir@gmail.com>
2023-10-27 21:25:41 +0400

    w

--- AVR/attiny85/blink/src/hello.c
+++ AVR/attiny85/blink/src/hello.c
@@ -1,46 +1,31 @@
-
-/**
- *  
- * Blinking LED ATTiny85 "hello world"
- *
- * Following the tutorial at:
- * http://www.instructables.com/id/Honey-I-Shrunk-the-Arduino-Moving-from-Arduino-t/?ALLSTEPS
- *  
- */
+// Muthanna Alwahash
+// October 2023
 
 #include <avr/io.h>
-// F_CPU frequency to be defined at command line
 #include <util/delay.h>
 
-// LED is on pin 2, PB3
-#define LED      PB5
-#define DELAY_MS 500
+#define LED      PB0
 
 int main () {
 	uint8_t high = 0;
 	uint16_t ms = 0;
 
-	// setup LED pin for output in port B's direction register
-	DDRB |= (1 << LED);
 
-	// set LED pin LOW
-	PORTB &= ~(1 << LED);
+	DDRB |= (1 << LED);			// setup LED pin for output in port B's direction register
+
+	
+	PORTB &= ~(1 << LED);		// set LED pin LOW
 
 	while (1) {	
 		high = !high;
 
 		if (high) {
-			// set LED pin HIGH
-			PORTB |= (1 << LED);
+			PORTB |= (1 << LED);	// set LED pin HIGH
 		} else {
-			// set LED pin LOW
-			PORTB &= ~(1 << LED);
+			PORTB &= ~(1 << LED);	// set LED pin LOW
 		}
 
-		// delay for 500 ms
-		for (ms = DELAY_MS; ms > 0; ms -= 10) {
-			_delay_ms(10);
-		}
+		_delay_ms(10);
 	}
 
 	return 0;
