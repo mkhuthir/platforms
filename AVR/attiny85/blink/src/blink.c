3cb04533ea1d4b85295c32c82a433f8f96514631
Muthanna Alwahash <mkhuthir@gmail.com>
2023-10-28 10:56:29 +0400

    w

--- AVR/attiny85/blink/src/blink.c
+++ AVR/attiny85/blink/src/blink.c
@@ -4,26 +4,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED0    PB0
+#define LED0    PB0				// will use bit 0 from Port B
+#define delay	100				// On period = Off period = 100ms
 
 int main () {
-	uint8_t  state = 0;
 
-	DDRB |= (1 << LED0);			// setup LED pin for output in port B's direction register
-	
-	PORTB &= ~(1 << LED0);			// set LED pin LOW
+	DDRB |= (1 << LED0);		// setup LED pin for output in PortB's direction register
 
 	while (1) {	
-		state = !state;
-
-		if (state) {
-			PORTB |= (1 << LED0);	// set LED pin HIGH
-
-		} else {
-			PORTB &= ~(1 << LED0);	// set LED pin LOW
-		}
-
-		_delay_ms(10);
+		PORTB |= (1 << LED0);	// set LED pin HIGH
+		_delay_ms(delay);		// delay
+		PORTB &= ~(1 << LED0);	// set LED pin LOW
+		_delay_ms(delay);		// delay
 	}
 
 	return 0;
