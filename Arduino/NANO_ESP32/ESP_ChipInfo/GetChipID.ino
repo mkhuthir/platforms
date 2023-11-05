bd67c0b9b26ce5b424f8f2efdb0ae1b62522fa14
Muthanna Alwahash <mkhuthir@gmail.com>
2023-11-05 09:42:07 +0400

    w

---
+++ Arduino/NANO_ESP32/ESP_ChipInfo/GetChipID.ino
@@ -0,0 +1,16 @@
+// Muthanna Alwahash
+// Nov 2023
+
+void setup() {
+	Serial.begin(115200);
+}
+
+void loop() {
+
+	Serial.printf("Chip model \t= %s\n", ESP.getChipModel());
+  Serial.printf("Revision \t= %d\n"  , ESP.getChipRevision());
+	Serial.printf("No of cores \t= %d\n",ESP.getChipCores());
+  
+	delay(3000);
+
+}
