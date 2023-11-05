a20c0ce0d43554ef9fe9e43b64052b6f29c82bf2
Muthanna Alwahash <mkhuthir@gmail.com>
2023-11-05 09:46:12 +0400

    w

--- Arduino/NANO_ESP32/blink/src/blink.cpp
+++ Arduino/NANO_ESP32/blink/src/blink.cpp
@@ -15,7 +15,7 @@ void setup()
 void loop()
 {
   digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                  // wait for a second
+  delay(500);                   // wait for a second
   digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                  // wait for a second
+  delay(500);                   // wait for a second
 }
