e93f9eb7a0f9679fd5a9894a69d10f103cff9526
Muthanna Alwahash <mkhuthir@gmail.com>
2023-11-05 17:33:05 +0400

    w

---
+++ Arduino/MKR1000/Blink/src/blink.cpp
@@ -0,0 +1,22 @@
+// Muthanna Alwahash
+// Nov 2023
+// LED Blink
+
+#include <Arduino.h>
+
+#ifndef LED_PIN
+    #define LED_PIN 6         // Set LED_PIN
+#endif
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);     // initialize LED digital pin as an output.
+}
+
+void loop()
+{
+  digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(500);                   // wait for a second
+  digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(500);                   // wait for a second
+}
