0224691f2888f66ac76aba255a324fd7f27912b3
Muthanna Alwahash <mkhuthir@gmail.com>
2023-11-08 21:47:42 +0400

    w

---
+++ Arduino/101/Blink/src/blink.cpp
@@ -0,0 +1,17 @@
+// Muthanna Alwahash
+// Nov 2023
+// LED Blink
+
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(50);                        // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(50);                        // wait for a second
+}
+
