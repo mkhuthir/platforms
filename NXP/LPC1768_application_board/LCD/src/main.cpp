e434223f2ae03bc86363ac789abe0f789f1720d6
Muthanna Alwahash <mkhuthir@gmail.com>
2023-11-19 07:00:13 +0400

    w

---
+++ NXP/LPC1768_application_board/LCD/src/main.cpp
@@ -0,0 +1,25 @@
+// Muthanna Alwahash
+// Nov 2023
+
+#include "mbed.h"
+#include "C12832.h"
+
+#define	delay_ms 150
+
+
+C12832 lcd(p5, p7, p6, p8, p11);
+
+int main()
+{
+    int j=0;
+    lcd.cls();
+    lcd.locate(0,3);
+    lcd.printf("mbed application board!");
+
+    while(true) {   // this is the third thread
+        lcd.locate(0,15);
+        lcd.printf("Counting : %d",j);
+        j++;
+        thread_sleep_for(delay_ms);
+    }
+}
