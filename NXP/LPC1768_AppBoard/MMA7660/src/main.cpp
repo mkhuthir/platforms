e854770216145231d3524ccddef3c44c9a0151dc
Muthanna Alwahash <mkhuthir@gmail.com>
2023-11-22 21:19:36 +0400

    w

--- NXP/LPC1768_AppBoard/MMA7660/src/main.cpp
+++ NXP/LPC1768_AppBoard/MMA7660/src/main.cpp
@@ -4,12 +4,12 @@
 #include "mbed.h"
 #include "MMA7660.h"
 
-MMA7660 acc(p28, p27);                  // initialize MMA7660 3-Axis Orientation/Motion Detection Sensor
+MMA7660 acc(p28, p27);  // initialize MMA7660 3-Axis Orientation/Motion Detection Sensor
 
 int main()
 {
     while(1) {
-        printf("ax=%+d\t\tay=%+d\t\taz=%+d\n",(int)(acc.x()*1000),(int)(acc.y()*1000),(int)(acc.z()*1000));
+        printf("ax=%+f\tay=%+f\taz=%+f\n",acc.x(),acc.y(),acc.z());
         thread_sleep_for(300);
     }
 }
