82fe8c96f9a8755d4212a1ddecaac876028873ed
Muthanna Alwahash <mkhuthir@gmail.com>
2023-12-03 20:45:22 +0400

    w

--- ESP/ESP8266_DSTIKE/Buzzer/src/main.cpp
+++ ESP/ESP8266_DSTIKE/Buzzer/src/main.cpp
@@ -1,20 +1,20 @@
 // Muthanna Alwahash
-// Nov 2023
-// LED Blink
+// Dec 2023
+
 
 #include <Arduino.h>
 
-#define LED_PIN 5          	// Set LED_PIN
+#define Buzzer_PIN 5          	// Buzzer Pin is on GPIO5
 
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);     // initialize LED digital pin as an output.
+  pinMode(Buzzer_PIN, OUTPUT);  // initialize digital pin as an output.
 }
 
 void loop()
 {
-  analogWrite(LED_PIN, 127);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                   // wait for a second
-  analogWrite(LED_PIN, 0);   // turn the LED off by making the voltage LOW
-  delay(500);                   // wait for a second
+  analogWrite(Buzzer_PIN, 127); // Send PWM signal at 50% duty cycle
+  delay(100);                   // wait 100ms
+  analogWrite(Buzzer_PIN, 0);   // Send PWM signal at 0% duty cycle
+  delay(500);                   // wait 100ms
 }
