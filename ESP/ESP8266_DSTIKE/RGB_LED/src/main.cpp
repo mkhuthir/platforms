339a6b651a9d326c3e93baa978fbdf32f0aff362
Muthanna Alwahash <mkhuthir@gmail.com>
2023-12-04 10:24:45 +0400

    w

---
+++ ESP/ESP8266_DSTIKE/RGB_LED/src/main.cpp
@@ -0,0 +1,14 @@
+// Muthanna Alwahash
+// Dec 2023
+
+#include <Arduino.h>
+#define SERIAL_BAUD 115200            // Baudrate for serial communication
+
+void setup() {
+  Serial.begin(SERIAL_BAUD);          // Start serial communication
+  Serial.println("Setup Done!\n");
+}
+
+void loop() {
+
+}
