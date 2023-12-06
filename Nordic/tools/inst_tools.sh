5347afbf78830a6556fa92db980d1f2ab31eed18
Muthanna Alwahash <mkhuthir@gmail.com>
2023-12-06 11:35:00 +0400

    w

--- Nordic/tools/inst_tools.sh
+++ Nordic/tools/inst_tools.sh
@@ -5,3 +5,11 @@ sudo apt install urjtag
 sudo apt install gdb-multiarch
 pip install -U pyocd
 sudo apt install device-tree-compiler
+
+# west
+wget https://apt.kitware.com/kitware-archive.sh
+sudo bash kitware-archive.sh
+sudo apt install --no-install-recommends git cmake ninja-build gperf \
+ccache dfu-util device-tree-compiler wget \
+python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
+make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1
