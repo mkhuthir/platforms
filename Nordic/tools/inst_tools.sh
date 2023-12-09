325a8cbaa41e86bdbcee1023cf712ad621709e93
Muthanna Alwahash <mkhuthir@gmail.com>
2023-12-09 20:10:09 +0400

    w

--- Nordic/tools/inst_tools.sh
+++ Nordic/tools/inst_tools.sh
@@ -3,12 +3,7 @@
 sudo apt install openocd urjtag gdb-multiarch 
 sudo apt install device-tree-compiler gcc 
 sudo apt install cmake make ninja-build
-sudo apt install 
-
 pip install -U pyocd
-
-
-
 
 # verify versions
 echo "cmake:\n"
