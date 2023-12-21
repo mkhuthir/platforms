998a4857c70c74a5356653f7be34855c7748f26c
Muthanna Alwahash <mkhuthir@gmail.com>
2023-12-21 21:24:39 +0400

    w

--- Nordic/nRF52832_Makerdiary_MDK/Fade_led/src/main.c
+++ Nordic/nRF52832_Makerdiary_MDK/Fade_led/src/main.c
@@ -5,31 +5,10 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 #include <device.h>
-#include <util/pwm.h>
-#include <board.h>
-
-#if defined(CONFIG_SOC_STM32F401XE) || defined(CONFIG_SOC_STM32L476XG)
-#define PWM_DRIVER CONFIG_PWM_STM32_2_DEV_NAME
-#define PWM_CHANNEL 1
-#elif CONFIG_SOC_STM32F103XB
-#define PWM_DRIVER CONFIG_PWM_STM32_1_DEV_NAME
-#define PWM_CHANNEL 1
-#elif defined(CONFIG_SOC_QUARK_SE_C1000) || defined(CONFIG_SOC_QUARK_D2000)
-#define PWM_DRIVER CONFIG_PWM_QMSI_DEV_NAME
-#define PWM_CHANNEL 0
-#elif defined(CONFIG_SOC_FAMILY_NRF)
+#include <drivers/pwm.h>
 #include <board.h>
 #define PWM_DRIVER CONFIG_PWM_NRF5_SW_0_DEV_NAME
 #define PWM_CHANNEL LED0_GPIO_PIN
-#elif defined(CONFIG_BOARD_HEXIWEAR_K64)
-#define PWM_DRIVER	GREEN_PWM_NAME
-#define PWM_CHANNEL	GREEN_PWM_CHANNEL
-#elif defined(CONFIG_SOC_ESP32)
-#define PWM_DRIVER CONFIG_PWM_LED_ESP32_DEV_NAME_0
-#define PWM_CHANNEL	21
-#else
-#error "Choose supported PWM driver"
-#endif
 
 /*
  * 50 is flicker fusion threshold. Modulated light will be perceived
