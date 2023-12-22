776784d6bb60978b60358de629f060abcf57939d
Muthanna Alwahash <mkhuthir@gmail.com>
2023-12-22 23:38:13 +0400

    w

--- Nordic/nRF52832_Makerdiary_MDK/BLE_Beacon/src/main.c
+++ Nordic/nRF52832_Makerdiary_MDK/BLE_Beacon/src/main.c
@@ -21,9 +21,9 @@ static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
 	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
 	BT_DATA_BYTES(BT_DATA_SVC_DATA16,
-		      0xaa, 0xfe, 	// Eddystone UUID
-		      0x10, 		// Eddystone-URL frame type
-		      0x00, 		// Calibrated Tx power at 0m
+		      0xaa, 0xfe, 							// Eddystone UUID
+		      0x10, 								// Eddystone-URL frame type
+		      0x00, 								// Calibrated Tx power at 0m
 		      0x00, 								// URL Scheme Prefix http://www.
 		      'z', 'e', 'p', 'h', 'y', 'r',			// zephyr
 		      'p', 'r', 'o', 'j', 'e', 'c', 't',    // project
