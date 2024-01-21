d9b85e8236ee0cefb5cbe2280304c6f7ae95c37f
Muthanna Alwahash <mkhuthir@gmail.com>
2024-01-21 20:35:08 +0400

    w

--- Microchip_PIC/PIC16F18855_MPLAB_Xpress_Eval_Board/Fade.X/src/main.c
+++ Microchip_PIC/PIC16F18855_MPLAB_Xpress_Eval_Board/Fade.X/src/main.c
@@ -7,20 +7,6 @@ void main(void)
 {
     SYSTEM_Initialize();    // initialize the device
     
-    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits, Use the following macros to:
-
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
-    
     while (1)               // execution loop
     {
 
