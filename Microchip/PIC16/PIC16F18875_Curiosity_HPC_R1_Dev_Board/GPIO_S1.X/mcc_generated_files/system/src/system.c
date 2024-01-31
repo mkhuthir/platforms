deaca98692c3c4fbc7fd1273ad6318440c7b9018
Muthanna Alwahash <mkhuthir@gmail.com>
2024-01-31 21:09:28 +0400

    w

---
+++ Microchip/PIC16/PIC16F18875_Curiosity_HPC_R1_Dev_Board/GPIO_S1.X/mcc_generated_files/system/src/system.c
@@ -0,0 +1,26 @@
+// Muthanna Alwahash
+// Jan 2024
+
+/**
+ * System Driver Source File
+ * 
+ * @file system.c
+ * 
+ * @ingroup systemdriver
+ * 
+ * @brief This file contains the API implementation for the System Driver.
+ *
+ * @version Driver Version 2.0.2
+*/
+
+#include "../system.h"
+
+
+void SYSTEM_Initialize(void)
+{
+    CLOCK_Initialize();
+    PIN_MANAGER_Initialize();
+    EUSART_Initialize();
+    INTERRUPT_Initialize();
+}
+
